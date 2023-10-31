c6a7c7b06d7d2e2993677c7f0d9e96476486905b
MoAfi12 <maxamedcaafi60@gmail.com>
2023-10-31 08:52:38 +0300

    last updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ const app = express()
 
 app.use(express.json())
 
-const whitelist = ['http://localhost:3000', 'http://localhost:4173', 'https://nodejs-deploy-6spo.onrender.com/'];
+const whitelist = ['http://localhost:3000', 'http://localhost:5173'];
 
 // Configure CORS middleware with whitelist
 const corsOptions = {
--- frontend/src/pages/CreateBooks.jsx
+++ frontend/src/pages/CreateBooks.jsx
@@ -1,5 +1,6 @@
 import axios from 'axios'
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 function CreateBooks() {
   const[name , setName] = useState('')
@@ -8,6 +9,7 @@ function CreateBooks() {
   const[publishYear , setPublishYear] = useState('')
   const[price , setPrice] = useState('')
 
+  const navigate = useNavigate()
 
 const handleSved = async()=>{
   try{
@@ -28,7 +30,7 @@ const handleSved = async()=>{
   else{
     const state = await axios.post("http://localhost:3000/api/books/register-book", data)
    console.log("the saved data" , state)
-
+   navigate("/list-books")
   }
 }
     catch(error){
