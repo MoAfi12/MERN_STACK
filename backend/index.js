8db2297c09dcf248696bff09ff887a3c612aa566
MoAfi12 <maxamedcaafi60@gmail.com>
2023-10-31 08:33:21 +0300

    last updated

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,24 @@ const app = express()
 
 app.use(express.json())
 
-app.use(cors())
+const whitelist = ['http://localhost:3000', 'http://localhost:4173'];
+
+// Configure CORS middleware with whitelist
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin)) {
+      callback(null, true); // Allow the request
+    } else {
+      callback(new Error('Not allowed by CORS')); // Deny the request
+    }
+  },
+};
+
+app.use(cors(corsOptions));
+
+
+var whil
+
 
 app.use('/api', useRouter)
   
