db5791509c352b44b6fad3427ec066b8b9a36173
MoAfi12 <maxamedcaafi60@gmail.com>
2023-10-31 09:52:14 +0300

    last updated

--- backend/index.js
+++ backend/index.js
@@ -7,18 +7,8 @@ const app = express()
 
 app.use(express.json())
 
-const whitelist = ['http://localhost:3000', 'http://localhost:5173', 'https://store-books.onrender.com/'];
-var corsOptionsDelegate = function (req, callback) {
-    var corsOptions;
-    if (whitelist.indexOf(req.header('Origin')) !== -1) {
-        corsOptions = { origin: true };  // Reflect (enable) the requested origin in the CORS response
-    } else {
-        corsOptions = { origin: false };  // Disable CORS for this request
-    }
-    callback(null, corsOptions);  // Callback expects two parameters: error and options
-};
 
-app.use(cors(corsOptionsDelegate));
+app.use(cors());
 
 app.use('/api', useRouter)
   
